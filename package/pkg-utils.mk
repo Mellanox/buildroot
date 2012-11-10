2ba56a305d7fa9a1c2c55422bad7fe5db30fa7d4
Noam Camus <noamc@ezchip.com>
2012-11-10 06:24:32 +0200

    package: change messege. some terminals do not understand tput e.g. in Eclipse. replace bold with asterisks.
    Signed-off-by: Noam Camus <noamc@ezchip.com>

--- package/pkg-utils.mk
+++ package/pkg-utils.mk
@@ -72,9 +72,8 @@ INFLATE.xz   = $(XZCAT)
 INFLATE.tar  = cat
 
 # MESSAGE Macro -- display a message in bold type
-MESSAGE = echo "$(TERM_BOLD)>>> $($(PKG)_NAME) $($(PKG)_VERSION) $(1)$(TERM_RESET)"
-TERM_BOLD := $(shell tput smso)
-TERM_RESET := $(shell tput rmso)
+MESSAGE_DELIM=*************************************************
+MESSAGE = echo -e "$(MESSAGE_DELIM) \n $($(PKG)_NAME) $($(PKG)_VERSION) $(1)\n$(MESSAGE_DELIM)"
 
 
 # Needed for the foreach loops to loop over the list of hooks, so that
